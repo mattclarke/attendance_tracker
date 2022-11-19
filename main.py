ce0fa3a7be049f2b6726bd7a78c641d20cb5984d
Matt Clarke <matt.clarke@esss.se>
2022-11-19 13:52:40 +0100

    moved window into a class and added a button

--- main.py
+++ main.py
@@ -2,39 +2,48 @@ import os
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QHeaderView
+from PyQt5.QtWidgets import QApplication, QHeaderView, QMainWindow
 
 from src.member_model import MemberModel
 
+basedir = os.path.dirname(__file__)
 
-def create_gui():
-    basedir = os.path.dirname(__file__)
-    window = uic.loadUi(os.path.join(basedir, "mainwindow.ui"))
-    model = MemberModel(
-        [
-            "Name",
-            "Birth year",
-            "Grade",
-            "Last graded",
-            "# lessons since grading",
-            "Notes",
-        ],
-        [0, 1],
-    )
-    model.insert_row()
-    model.insert_row()
-    window.table_members.setModel(model)
-    window.table_members.horizontalHeader().setSectionResizeMode(
-        QHeaderView.Interactive
-    )
-    window.table_members.horizontalHeader().setStretchLastSection(True)
-    window.table_members.resizeColumnsToContents()
-    return window
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        uic.loadUi(os.path.join(basedir, "mainwindow.ui"), self)
+        self.btn_import.clicked.connect(self.import_data)
+        self._initialise_table()
+
+    def _initialise_table(self):
+        self.model = MemberModel(
+            [
+                "Name",
+                "Birth year",
+                "Grade",
+                "Last graded",
+                "# lessons since grading",
+                "Notes",
+            ],
+            [0, 1],
+        )
+        self.model.insert_row()
+        self.model.insert_row()
+        self.table_members.setModel(self.model)
+        self.table_members.horizontalHeader().setSectionResizeMode(
+            QHeaderView.Interactive
+        )
+        self.table_members.horizontalHeader().setStretchLastSection(True)
+        self.table_members.resizeColumnsToContents()
+
+    def import_data(self):
+        pass
 
 
 if __name__ == "__main__":
     basedir = os.path.dirname(__file__)
     app = QApplication(sys.argv)
-    window = create_gui()
+    window = MainWindow()
     window.show()
     app.exec_()
