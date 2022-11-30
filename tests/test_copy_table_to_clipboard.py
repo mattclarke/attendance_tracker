34b5880670b7d84b5260e54b049644dc09929e27
Matt Clarke <matt.clarke@esss.se>
2022-11-30 15:28:01 +0100

    Added test for skipping empty row

--- tests/test_copy_table_to_clipboard.py
+++ tests/test_copy_table_to_clipboard.py
@@ -1,3 +1,5 @@
+from PyQt5.QtCore import Qt
+
 from src.member_model import MemberModel
 
 
@@ -22,3 +24,17 @@ def test_can_copy_table_data_to_clipboard():
         result
         == "Adam\t1977\t\t\t\t5\t\nBea\t1987\t\t\t\t15\t\nCarlo\t1997\t\t\t\t25\t"
     )
+
+
+def test_blank_lines_skipped():
+    model = MemberModel()
+    model.update_members(
+        [("Adam", "1977", 5), ("Bea", "1987", 15), ("Carlo", "1997", 25)]
+    )
+
+    for col in range(model.columnCount()):
+        model.setData(model.createIndex(1, col), "", Qt.ItemDataRole.EditRole)
+
+    result = convert_table_to_clipboard_format(model)
+
+    assert result == "Adam\t1977\t\t\t\t5\t\nCarlo\t1997\t\t\t\t25\t"
